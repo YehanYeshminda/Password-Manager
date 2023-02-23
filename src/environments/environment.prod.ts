9a7ee2199ca97b4d34b6db9c11502c5fdc2b5850
Yehan Yeshminda <yeshanyesh12@gmail.com>
2023-02-23 21:15:07 +0530

    added styling for site list

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'password-manager-53e12',
+    appId: '1:672485374399:web:905085d09967b9157fc90a',
+    databaseURL: 'https://password-manager-53e12-default-rtdb.firebaseio.com',
+    storageBucket: 'password-manager-53e12.appspot.com',
+    apiKey: 'AIzaSyCxZBe6R8GQWBxju2D3pLnxoY9L3ZE3XoQ',
+    authDomain: 'password-manager-53e12.firebaseapp.com',
+    messagingSenderId: '672485374399',
+  },
   production: true
 };
